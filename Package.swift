bfbad06e6914e5b0d94259ceb6ffdf9b2145a3a4
kevinschlieper <kevin@bottledsoftware.de>
2017-11-07 12:20:20 +0100

    Update Package.swift
    Update Package.swift for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,19 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "MySQLDriver",
+    products: [
+        .library(name: "MySQLDriver", targets: ["MySQLDriver"])
+    ],
     dependencies: [
-    ]
+    ],
+    targets: [
+        .target(
+            name: "MySQLDriver",
+            dependencies: [
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [4]
 )
-
-
-
